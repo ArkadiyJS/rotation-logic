0ed18bcae31c66e979b73cc8499882ed657b1c25
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-11-05 11:32:30 +0500

    start skelet app

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search(props) {
+  return (
+    <div>
+      <input className='Input' placeholder='Найти товар...' />
+    </div>
+  );
+}
+
+export default Search;
