8ec654c57e6e63d2a5f2c7b5cc8b4f45d3bc6922
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-11-05 15:09:55 +0500

    npm axios and first request DB

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
 import List from './Components/List/List';
 import Search from './Components/Search/Search';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios.get('https://636626dc79b0914b75ca7a73.mockapi.io/data').then((res) => { setItems(res.data) })
+  }, [])
+
+  console.log(items)
+
   return (
     <div className="App">
       <h1> Ez Rotation</h1>
